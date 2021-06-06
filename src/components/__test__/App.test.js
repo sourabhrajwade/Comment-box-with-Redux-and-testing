bbaa9da6b13cfa79c0a5f962b828a6e4492e3aff
Sourabh Rajwade <rajwadesourabh@gmail.com>
2021-06-06 16:43:13 +0530

    test app refactoring

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<App />);
+});
 
 it('shows a comment box', () => {
-    const wrapper = shallow(<App />);
-
     expect(wrapper.find(CommentBox).length).toEqual(1);
-})
+});
 
 it('shows a comment list', () => {
-    const wrapper = shallow(<App />);
-
     expect(wrapper.find(CommentList).length).toEqual(1);
-})
+});
